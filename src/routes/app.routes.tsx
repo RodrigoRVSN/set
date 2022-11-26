e3d7343c3e07bd9b74e90bff3375307f4aaf25d0
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-11-26 15:42:21 -0300

    created first condominium register template

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import Menu from "../views/Menu";
 import { About } from "../views/About";
 import Condominiums from "../views/Condominiums";
 import Syndicates from "../views/Syndicates";
+import CondominiumRegister from "../views/Condominiums/CondominiumRegister/CondominiumRegister";
 
 const AppStack = createNativeStackNavigator()
 
@@ -13,7 +14,7 @@ const AppRoutes: React.FC = () => (
     <AppStack.Screen name='Menu' options={{ headerShown: false }} component={Menu} />
     <AppStack.Screen name='Dashboard' component={Dashboard} />
     <AppStack.Screen name='Condomínios' component={Condominiums} />
-    <AppStack.Screen name='Registrar Condomínio' component={Condominiums} />
+    <AppStack.Screen name='Registrar Condomínio' component={CondominiumRegister} />
     <AppStack.Screen name='Síndicos' component={Syndicates} />
     <AppStack.Screen name='Sobre' component={About} />
   </AppStack.Navigator>
