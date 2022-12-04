51064c54b9cb8869928460e47f68c933ef008b95
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-12-03 21:09:41 -0300

    feat: add dashboard in WebView

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -3,12 +3,15 @@ import { View, Text } from "react-native";
 import * as Animatable from 'react-native-animatable'
 import { commonStyles } from "../../Styles/commonStyles";
 import { styles } from "./styles";
+import { WebView } from 'react-native-webview';
 
 export default function Dashboard() {
   return (
     <View style={{backgroundColor: commonStyles.primaryColor}}>
     <Animatable.View style={styles.mainContainer} delay={600} animation='fadeInUp'>
-      <Text>Dashboard</Text>
+      <WebView
+        source={{ uri: 'https://nata-vito-solar-forecasting-main-io221k.streamlit.app/' }}
+      />
     </Animatable.View>
     </View>
   )
